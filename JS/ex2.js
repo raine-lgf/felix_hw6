29d960ea09181c60bc65b3b91f750586c896ca4d
Raine Felix <rainefelix.lgf@gmail.com>
2020-10-13 15:39:50 -0700

    commit

---
+++ JS/ex2.js
@@ -0,0 +1,8 @@
+/* HOMEWORK 6 Example 2 JavaScript Code */
+console.log("Raine's Output from HOMEWORK 6 Example 2");
+
+
+const displayText400 = document.getElementsByClassName("400level").textContext;
+
+console.log(document.getElementById("spring").getElementsByClassName("400level").innerHTML);
+console.log(displayText400);
---
+++ JS/ex3.js
@@ -0,0 +1,2 @@
+/* HOMEWORK 6 Example 3 JavaScript Code */
+console.log("Raine's Output from HOMEWORK 6 Example 3");
