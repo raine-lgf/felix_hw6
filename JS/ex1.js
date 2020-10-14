38bb1217ceb1972a7c39210b81a5526c3356aab8
Raine Felix <rainefelix.lgf@gmail.com>
2020-10-13 18:13:46 -0700

    commit

---
+++ JS/ex1.js
@@ -0,0 +1,12 @@
+/* HOMEWORK 6 Example 1 JavaScript Code */
+console.log("Raine's Output from HOMEWORK 6 Example 1");
+
+//Complete the following functions --> Displays ALL nodes
+const bodyNodes = document.body;
+
+// Browse the body node's children using the forEach() method
+document.body.childNodes.forEach(node => {
+    console.log(node);
+});
+
+console.log(document.body.bodyNodes);
